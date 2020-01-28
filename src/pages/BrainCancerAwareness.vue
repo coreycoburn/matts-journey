cb0524602a4021c5f16a5763c3a8c3f3c6169ef1
Corey Coburn <corey@coburn.codes>
2020-01-28 10:40:50 -0800

    Added default container to stub pages

--- src/pages/BrainCancerAwareness.vue
+++ src/pages/BrainCancerAwareness.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <h1>Brain Cancer Awareness</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
   </div>
