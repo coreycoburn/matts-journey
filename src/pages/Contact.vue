daca6a028ceea2fff90c085f6b3846590132d669
Corey Coburn <corey@coburn.codes>
2020-01-28 12:17:05 -0800

    Added contact info and contact from ui
    The form is not implemented with Netlify yet.

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,9 +1,50 @@
 <template>
   <div class="container mx-auto max-w-2xl">
-    <h1>Contact</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>Contact Event Coordinators</h1>
+    <p>
+      If you have questions or for more information you may fill out the following form, send an email or call one of the event coordinators for the {{ $static.metadata.eventName }} event.
+    </p>
+    <div class="mb-4">
+      <h2>Coordinator Contact Info</h2>
+      <ul>
+        <li>Email: <a :href="`mailto:${$static.metadata.email}`" v-text="$static.metadata.email" /></li>
+        <li><strong>Lisa Tarner:</strong> 707-227-6095</li>
+        <li><strong>Diane Bishofberger:</strong> 707-227-2234</li>
+        <li><strong>Ruth Appleby:</strong> 707-227-6123</li>
+      </ul>
+    </div>
+    <form class="mb-8">
+      <div>
+        <label for="name" class="text-base">Name:</label>
+        <input type="text" id="name" name="name" placeholder="Full Name" class="w-full px-4 py-2 text-base appearance-none border-2 border-gray-400 bg-gray-200 rounded-lg" aria-required="true">
+      </div>
+      <div>
+        <label for="phone" class="text-base">Phone:</label>
+        <input type="text" id="phone" name="phone" placeholder="Phone" class="w-full px-4 py-2 text-base appearance-none border-2 border-gray-400 bg-gray-200 rounded-lg" aria-required="true">
+      </div>
+      <div>
+        <label for="email" class="text-base">Email:</label>
+        <input type="text" id="email" name="email" placeholder="Email" class="w-full px-4 py-2 text-base appearance-none border-2 border-gray-400 bg-gray-200 rounded-lg" aria-required="true">
+      </div>
+      <div>
+        <label for="message" class="text-base">Message:</label>
+        <textarea id="message" name="message" class="w-full px-4 py-2 text-base appearance-none border-2 border-gray-400 bg-gray-200 rounded-lg" />
+      </div>
+      <div class="flex justify-center">
+        <button class="button">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
+
+<static-query>
+query {
+  metadata {
+    email,
+    eventName,
+  }
+}
+</static-query>
 
 <script>
 export default {
