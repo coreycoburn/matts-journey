1d8a741e455efc92cb8d44832f0172b1bf3427ee
Corey Coburn <corey@coburn.codes>
2020-02-06 14:42:13 -0800

    Added open graph and twitter cards

--- src/main.js
+++ src/main.js
@@ -3,11 +3,70 @@ import LoadScript from 'vue-plugin-load-script'
 import Vuelidate from 'vuelidate'
 import VueMask from 'v-mask'
 
-export default function (Vue, { appOptions }) {
+export default function (Vue, { appOptions, head }) {
   Vue.use(Vuex)
   Vue.use(LoadScript)
   Vue.use(Vuelidate)
   Vue.use(VueMask)
+
+  head.meta.push([
+    {
+      name: 'keywords',
+      content: 'tumor,brain,cancer,fundraiser,event,Yountville,Napa,Matt Appleby',
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, user-scalable=no',
+    },
+    {
+      property: 'og:site_name',
+      content: 'Help Matt Appleby',
+    },
+    {
+      name: 'description',
+      content: 'Help Matt Appleby and his family fight offset some of his medical expenses that are not covered by his insurance and to ease the burden caused by his dramatically reduced income.',
+    },
+    {
+      property: 'og:title',
+      content: 'Help Matt Appleby',
+    },
+    {
+      property: 'og:description',
+      content: 'Help Matt Appleby and his family fight offset some of his medical expenses that are not covered by his insurance and to ease the burden caused by his dramatically reduced income.',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:url',
+      content: 'https://www.helpmattappleby.com',
+    },
+    {
+      property: 'og:image',
+      content: 'https://www.helpmattappleby.com/field_of_hope_logo.jpg',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      name: 'twitter:creator',
+      content: '@coreycoburn',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Help Matt Appleby',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Help Matt Appleby and his family fight offset some of his medical expenses that are not covered by his insurance and to ease the burden caused by his dramatically reduced income.',
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://www.helpmattappleby.com/field_of_hope_logo.jpg',
+    },
+  ])
 
   appOptions.store = new Vuex.Store({
     state: {
